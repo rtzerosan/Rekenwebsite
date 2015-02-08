4d4be64534a4249f31e2acd87e5ce53f5baa16ce
rtzerosan <rtzerosan@gmail.com>
2015-02-08 15:08:07 +0100

    Initial commit

---
+++ toetsnakijken.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+
+$getal1 = $_POST['getal1'];
+$getal2 = $_POST['getal2'];
+
+$som = $getal1 + $getal2;
+
+array_push($_SESSION['sommen'], $som);
+
+echo $getal1;
+
+
+
+
+
+
+
+
+header("location:Toets.php");
+
+?>
