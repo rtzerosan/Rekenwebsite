52deca4844e0aa7b671e4c39555d9f5db4620785
rtzerosan <rtzerosan@gmail.com>
2015-02-08 13:43:53 +0100

    Initial commit

--- Toets.php
+++ Toets.php
@@ -1,5 +1,28 @@
 <?php
-if(isset($_SESSION['teller']))
+SESSION_START()
+if(isset($_SESSION['nummer']))
 {
-$teller=$_SESSION['teller'];
+	$nummer=$_SESSION['nummer'];
+}
+else
+{
+	$nummer=0;
+}
+if(isset($_SESSION['punt'];
+{
+	$punt=$_SESSION['punt'];
+}
+else
+{
+	$punt=1;
+}
+if($nummer<=10)
+{
+
+}
+else
+{
+	echo" gefeliciteerd, U hebt een $punt gehaald";
+	SESSION_DESTROY()
+	echo" Wilt u nog een toets maken? <a href='toets.php'>
 }
