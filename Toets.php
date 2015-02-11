26d1f5473e427d3f70c02c1c419d627891ef4965
rtzerosan <rtzerosan@gmail.com>
2015-02-11 14:42:22 +0100

    Initial commit

--- Toets.php
+++ Toets.php
@@ -1,9 +1,11 @@
 <?php
 SESSION_START();
-if(!isset($_SESSION['answer']))
+
+if(!isset($_SESSION['results']))
 {
-$_SESSION['answer']=array();
+$_SESSION['results']=array();
 }
+
 
 if(isset($_SESSION['teller']))
 {
@@ -14,36 +16,34 @@ else
 	$teller=1;
 }
 
-if($teller<=5)
+if($teller<=20)
 {
 	$teller=$teller+1;
 	$_SESSION['teller']=$teller;
 	$_SESSION['getal1']=rand(1,10);
 	$_SESSION['getal2']=rand(1,10);
-
+	$_SESSION['efficient']=rand(1,4);
+	
 	$getal1 = $_SESSION['getal1'];
 	$getal2 = $_SESSION['getal2'];
-
-
-	$_SESSION['efficient']=rand(1,4);
-
 	$efficient = $_SESSION['efficient'];
+
+	
 	echo "<form action='toetsnakijken.php' method='post'>";
 	if($efficient == 1){
 		$antwoord=$getal1+$getal2;
 		echo"$getal1 + $getal2 = <input type='text' name='answer' /><br /> ";
 	}
 	elseif($efficient == 2){
-		if($getal1>$getal2)
+		if($getal2>$getal1)
 		{
+		$getal3=$getal1;
+		$getal1=$getal2;
+		$getal2=$getal3;
+		}
 			$antwoord=$getal1-$getal2;
 			echo"$getal1 - $getal2 =  <input type='text' name='answer' /><br />";
-		}
-		else
-		{
-			$antwoord=$getal2-$getal1;
-			echo"$getal2 - $getal1 =  <input type='text' name='answer' /><br />";
-		}
+		
 		
 	}
 
@@ -54,10 +54,16 @@ if($teller<=5)
 
 	else{
 		$getal3=$getal1*$getal2;
-		$antwoord=$getal2;
-		echo"$getal3 / $getal1 = <input type='text' name='answer' /><br />";
+		
+		$getal4=$getal1;
+		$getal1=$getal3;
+		$getal3=$getal4; 
+		
+		$antwoord=$getal3;
+		echo"$getal1 / $getal2 = <input type='text' name='answer' /><br />";
 	}
-	$efficient = $_SESSION['efficient'];
+	echo count($_SESSION['results']);
+	$_SESSION['antwoord']=$antwoord;
 	echo "	<input type='submit' name='volgende' value='klik voor volgende vraag' />";
 	echo "<input type='hidden' name='getal1' value='$getal1' />
 		<input type='hidden' name='getal2' value='$getal2' />";
@@ -65,18 +71,27 @@ if($teller<=5)
 	}
 else
 {
-	$results=$_SESSION['answer'];
+$reveal=0;
+
+$results=$_SESSION['results'];
+echo"<table>"; 
+for($reader=1 ;$reader<=7; $reader++)
+{
+echo"<td>";
 
 	foreach($results as $resultstrue)
 	{
-		echo" $resultstrue<br />";
+		echo $resultstrue[$reveal];
+		
+		echo" <br />";
+	}	
+	$reveal++;
+	echo "</td>";
 	}
-	
-	session_destroy();
-	
+echo"</table>";
 	echo" Einde van toets";
 
-
+session_destroy();
 }
 
 ?>
--- toetsnakijken.php
+++ toetsnakijken.php
@@ -4,11 +4,38 @@ session_start();
 
 $getal1 = $_POST['getal1'];
 $getal2 = $_POST['getal2'];
-$answero = $_POST['answer'];
-$answer = $_SESSION['answer'];
-array_push($answer, $answero);
-$_SESSION['answer']=$answer;
-$som = $getal1 + $getal2;
+$answer = $_POST['answer'];
+$results = $_SESSION['results'];
+$antwoord = $_SESSION['antwoord'];
+$efficient = $_SESSION['efficient'];
+if($answer==$antwoord)
+{
+$controle='goed';
+}
+else
+{
+$controle='fout';
+}
+
+if ($efficient==1)
+
+	{$soort='+';}
+	
+elseif ($efficient==2)
+
+	{$soort='-';}
+	
+elseif ($efficient==3)
+
+	{$soort='*';}
+	
+else
+
+	{$soort='/';}
+	
+array_push($results, array($getal1, $soort, $getal2, '=', $answer, $antwoord, $controle));
+
+$_SESSION['results']=$results;
 echo $getal1;
 header("location:Toets.php");
 
