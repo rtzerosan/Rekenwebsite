4cf8150310fcf880a81fc755fca470d30639dc78
rtzerosan <rtzerosan@gmail.com>
2015-02-13 11:24:42 +0100

    Initial commit

---
+++ Toets6nakijken.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+
+$getal1 = $_POST['getal1'];
+$getal2 = $_POST['getal2'];
+$answer = $_POST['answer'];
+$results = $_SESSION['results'];
+$antwoord = $_SESSION['antwoord'];
+$efficient = $_SESSION['efficient'];
+$punt = $_SESSION['punt'];
+if($answer==$antwoord)
+{
+$controle='goed';
+}
+else
+{
+$controle='fout';
+$punt=$punt-0.45;
+}
+
+if ($efficient==1)
+
+	{$soort='+';}
+	
+elseif ($efficient==2)
+
+	{$soort='-';}
+	
+elseif ($efficient==3)
+
+	{$soort='*';}
+	
+elseif ($efficient==4)
+
+	{$soort='/';}
+else
+	{$soort=wortel}
+$_SESSION['punt']=$punt;
+	
+array_push($results, array($getal1, $soort, $getal2, '=', $answer, $antwoord, $controle));
+
+$_SESSION['results']=$results;
+echo $getal1;
+header("location:home.php?pagina=5");
+
+?>
