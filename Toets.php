a6a534bbc6a235e09f8aeb116b54ec25c84d5c15
rtzerosan <rtzerosan@gmail.com>
2015-02-13 09:56:44 +0100

    Initial commit

--- Toets.php
+++ Toets.php
@@ -15,7 +15,14 @@ else
 {
 	$teller=1;
 }
-
+if(!isset($_SESSION['punt']))
+{
+	$punt=10;
+}
+else
+{
+	$punt=$_SESSION['punt'];
+}
 if($teller<=20)
 {
 	$teller=$teller+1;
@@ -23,17 +30,21 @@ if($teller<=20)
 	$_SESSION['getal1']=rand(1,10);
 	$_SESSION['getal2']=rand(1,10);
 	$_SESSION['efficient']=rand(1,4);
+	$_SESSION['punt']=$punt;
 	
 	$getal1 = $_SESSION['getal1'];
 	$getal2 = $_SESSION['getal2'];
 	$efficient = $_SESSION['efficient'];
-
+	$vraagnummer=count($_SESSION['results'])+1;
 	
 	echo "<form action='toetsnakijken.php' method='post'>";
+	echo "Vraag nummer $vraagnummer <br />";
+	echo" <br />";
 	if($efficient == 1){
 		$antwoord=$getal1+$getal2;
 		echo"$getal1 + $getal2 = <input type='text' name='answer' /><br /> ";
-	}
+
+		}
 	elseif($efficient == 2){
 		if($getal2>$getal1)
 		{
@@ -62,8 +73,10 @@ if($teller<=20)
 		$antwoord=$getal3;
 		echo"$getal1 / $getal2 = <input type='text' name='answer' /><br />";
 	}
-	echo count($_SESSION['results']);
+	
+	
 	$_SESSION['antwoord']=$antwoord;
+			echo" <br />";
 	echo "	<input type='submit' name='volgende' value='klik voor volgende vraag' />";
 	echo "<input type='hidden' name='getal1' value='$getal1' />
 		<input type='hidden' name='getal2' value='$getal2' />";
@@ -71,10 +84,20 @@ if($teller<=20)
 	}
 else
 {
+$punt=$_SESSION['punt'];
 $reveal=0;
-
 $results=$_SESSION['results'];
+echo" Proficiat, je hebt een $punt gehaald";
 echo"<table>"; 
+echo"<tr>";
+echo"<th></th>";
+echo"<th>Som</th>";
+echo"<th></th>";
+echo"<th></th>";
+echo"<th> Jouw antwoord</th>";
+echo"<th> Het antwoord</th>";
+echo"<th> Goed of fout?</th>";
+echo"</tr> <tr>";
 for($reader=1 ;$reader<=7; $reader++)
 {
 echo"<td>";
@@ -88,6 +111,7 @@ echo"<td>";
 	$reveal++;
 	echo "</td>";
 	}
+echo"</tr>";
 echo"</table>";
 	echo" Einde van toets";
 
